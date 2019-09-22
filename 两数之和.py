f6f90e337286d962b58ecfb418eaf836713e254a
shiyin-lc <55621958+shiyin-lc@users.noreply.github.com>
2019-09-22 10:35:16 +0800

    Create 两数之和.py

---
+++ 两数之和.py
@@ -0,0 +1,32 @@
+"""
+  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_list = enumerate(nums)
+        for index, num in num_list:
+            another = target - num
+            if another in nums:
+                find_index = nums.index(another)
+                if find_index != index:
+                    return [index, find_index]
+# 主要方法是判断target-num是否在列表中
+# list.index(obj) 可以直接返回元素所在下标
